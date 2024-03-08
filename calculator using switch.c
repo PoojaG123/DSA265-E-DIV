819cb89a7416e4cc9a8578ffa08ce6ef364fcd50
PoojaG123 <119643070+PoojaG123@users.noreply.github.com>
2024-03-08 14:02:23 +0530

    Add files via upload

---
+++ calculator using switch.c
@@ -0,0 +1,40 @@
+//modular c program for calculator using switch
+/*
+  Name:Pooja V gadad
+  Roll No: 521
+  Date :08/03/2024
+  Div: E
+*/#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    char operation;
+    float result;
+    printf("Enter 2 numbers\n ");
+
+    scanf("%d%d",&num1,&num2);
+    printf("Enter the operation\n");
+    scanf(" %c",&operation);
+    switch(operation)
+    {
+        case'+': result=num1+num2;
+                 printf("sum is %0.2f\n",result);
+                 break;
+
+        case'-': result=num1-num2;
+                 printf("Subtraction is %0.2f\n",result);
+                 break;
+
+        case'*': result=num1*num2;
+                 printf("Multiplication is %0.2f\n",result);
+                 break;
+
+        case'/': result=(float)num1/num2;
+                 printf("Division is %0.2f\n",result);
+                 break;
+
+        default : printf("Invalid operation\n");
+                  break;
+    }
+
+}
