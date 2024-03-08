05bb32d7e6bee5e8a501f9dabb31bdfe663bacc1
PoojaG123 <119643070+PoojaG123@users.noreply.github.com>
2024-03-08 12:10:17 +0530

    Add files via upload

---
+++ print message 5times.c
@@ -0,0 +1,21 @@
+//modular c program to display a message 5 times
+/*
+  Name:Pooja V gadad
+  Roll No: 521
+  Date :08/03/2024
+  Div: E
+*/
+#include<stdio.h>
+//function prototype
+void print_message(int i);
+int main()
+{
+    int i;
+    i=1;
+    while(i<=5)
+    {
+        printf("WELCOME TO C LAB\n");
+        i++;
+    }
+}
+
