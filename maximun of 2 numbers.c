e811c28bcf50d1380276fad11d0b5ecdaa800d84
PoojaG123 <119643070+PoojaG123@users.noreply.github.com>
2024-03-08 11:17:25 +0530

    Add files via upload

---
+++ maximun of 2 numbers.c
@@ -0,0 +1,20 @@
+//C program to check maximum of two numbers
+/*Name:Pooja V Gadad
+  Roll No:521
+  Division:E
+  Date:08/03/2024
+*/
+ #include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a and b values\n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+        printf("%d is maximum\n",a);
+    else
+        printf("%d is maximum\n",b);
+    printf("This program checks maximum of two numbers");
+    return 0;
+}
+
