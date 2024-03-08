8c20bb603e04af538145a81bd36819584c616585
PoojaG123 <119643070+PoojaG123@users.noreply.github.com>
2024-03-08 12:07:05 +0530

    Add files via upload

---
+++ Number is 99 or No.c
@@ -0,0 +1,25 @@
+//C Program to check number is 99
+/*Name:Pooja V gadad
+  Roll no:521
+  Date:08/03/2024
+  Division:E
+*/
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a value of num\n");
+    scanf("%d",&num);
+    if(num==99)
+    {
+        printf("Number is %d\n",num);
+        printf("Number is equal to 99\n");
+    }
+    else
+    {
+        printf("Number is %d\n",num);
+        printf("number is not 99\n");
+    }
+    return 0;
+}
+
