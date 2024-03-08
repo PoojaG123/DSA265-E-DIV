e995fc4fbbcedc25241a1ce8f817c642e10b169a
PoojaG123 <119643070+PoojaG123@users.noreply.github.com>
2024-03-08 14:53:46 +0530

    Add files via upload

---
+++ Ascii value.c
@@ -0,0 +1,16 @@
+//modular c program to print ascii value
+/*
+  Name:Pooja V gadad
+  Roll No: 521
+  Date :08/03/2024
+  Div: E
+*/
+#include<stdio.h>
+int main()
+{
+ char ch;
+ printf("Enter any chacater: ");
+ scanf("%c",&ch);
+ printf("ASCII value of charcter %c is :%d",ch,ch);
+ return 0;
+}
